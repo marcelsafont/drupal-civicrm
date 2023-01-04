41bc91a73b344eff59f628046ae2d4dd787205da
marcelsafont <marcel.safont1983@gmail.com>
2023-01-04 14:04:45 +0100

    added gd

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4f1cab64ddb5818c00bb2eea428b115b7d528241',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4f1cab64ddb5818c00bb2eea428b115b7d528241',
+    ),
+  ),
+);
